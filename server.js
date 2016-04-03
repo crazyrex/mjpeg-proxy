29fd1d71259140254a57b71d311e59fa2c206da3
Luigi Bifulco <bifulcoluigi@gmail.com>
2016-04-03 18:57:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var MjpegProxy = require('mjpeg-proxy').MjpegProxy;
 var express = require('express');
 var app = express();
- 
-app.get('/stream.mjpg', new MjpegProxy('http://admin:manga123@10.0.0.4/videostream.cgi?&loginuse=admin&loginpas=manga123').proxyRequest);
+var url = '<put here test url>' 
+app.get('/stream.mjpg', new MjpegProxy(url).proxyRequest);
 app.listen(8082);
